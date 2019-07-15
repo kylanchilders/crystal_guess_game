657ce8ea8f5f431028f4c34f2825fa541e1d36e0
T3k5upp0rt <kylan.childers@gmail.com>
2019-07-14 18:45:48 -0700

    created normal function instead of onclick function for new game and set it as a method in each of my onclick events for each crystal. Created if statement in each crystal to check if new total after click is greater than the number to guess for a lose, or if new total after click is equal to the total to guess, which results in a win.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,9 @@ $(document).ready(function(){
     var crystal4 = Math.floor(Math.random() * 10) + 1
     var currentTotal = 0
 
-    $('#start-button').on('click', function() {
+    $('#total-to-guess').html(totalToGuess)
+
+    function startGame() {
         totalToGuess = Math.floor(Math.random() * 100) + 1
         crystal1 = Math.floor(Math.random() * 10) + 1
         crystal2 = Math.floor(Math.random() * 10) + 1
@@ -17,27 +19,68 @@ $(document).ready(function(){
         crystal4 = Math.floor(Math.random() * 10) + 1
         currentTotal = 0
         $('#current-total').html(currentTotal)
-        $('#total-to-guess').html(totalToGuess)   
+        $('#total-to-guess').html(totalToGuess) 
+        console.log("crystal1: " + crystal1)  
+        console.log("crystal2: " + crystal2)
+        console.log("crystal3: " + crystal3)
+        console.log("crystal4: " + crystal4)
+    }
+
+
+    $('#start-button').on('click', function() {
+        startGame()
     });
 
     $('#crystal1').on('click', function() {
         currentTotal = currentTotal + crystal1
         $('#current-total').html(currentTotal);
+        if (currentTotal > totalToGuess){
+            alert("GAME OVER! YOU LOSE!")
+            startGame()
+        }
+        if (currentTotal == totalToGuess){
+            alert("YOU WIN! GOOD GAME!")
+            startGame()
+        }
     });
 
     $('#crystal2').on('click', function() {
         currentTotal = currentTotal + crystal2
         $('#current-total').html(currentTotal);
+        if (currentTotal > totalToGuess){
+            alert("GAME OVER! YOU LOSE!")
+            startGame()
+        }
+        if (currentTotal == totalToGuess){
+            alert("YOU WIN! GOOD GAME!")
+            startGame()
+        }
     });
 
     $('#crystal3').on('click', function() {
         currentTotal = currentTotal + crystal3
         $('#current-total').html(currentTotal);
+        if (currentTotal > totalToGuess){
+            alert("GAME OVER! YOU LOSE!")
+            startGame()
+        }
+        if (currentTotal == totalToGuess){
+            alert("YOU WIN! GOOD GAME!")
+            startGame()
+        }
     });
 
     $('#crystal4').on('click', function() {
         currentTotal = currentTotal + crystal4
         $('#current-total').html(currentTotal);
+        if (currentTotal > totalToGuess){
+            alert("GAME OVER! YOU LOSE!")
+            startGame()
+        }
+        if (currentTotal == totalToGuess){
+            alert("YOU WIN! GOOD GAME!")
+            startGame()
+        }
     });
 
 
